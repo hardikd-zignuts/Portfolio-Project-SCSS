959108e71b79f42e6c49d2147904eae2494859e2
hardikd-zignuts <hardikd@zignuts.com>
2023-02-06 15:58:35 +0530

    created menu of hamburger

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,5 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const burger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
 
 let menuShow = false;
 
@@ -8,9 +10,13 @@ menuBtn.addEventListener('click', toggleMenu)
 function toggleMenu() {
     if (!menuShow) {
         burger.classList.add('open')
+        nav.classList.add('open');
+        menuNav.classList.add('open');
         menuShow = true;
     } else {
         burger.classList.remove('open')
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
         menuShow = false;
     }
 }
